8735925535b74f19aaed15413831c4baec925607
mauLovera <mauricioloveraedu@gmail.com>
2022-06-06 12:07:20 -0400

    add responsive breakpoints

---
+++ js/app.js
@@ -0,0 +1,16 @@
+/*----Constants----------------------------------*/
+
+const hamburger = document.querySelector('#hamburger')
+
+/*----Variables----------------------------------*/
+
+/*----Cashed Element References------------------*/
+
+hamburger.addEventListener(`click`, sanity) 
+
+/*----Event Listeners----------------------------*/
+/*----Functions----------------------------------*/
+
+function sanity () {
+  console.log(`sanity`)
+}
